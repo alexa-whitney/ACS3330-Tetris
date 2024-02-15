1415309c10cb85f9de12c82aed35646a58848e2e
alexa-whitney <alexawhitney@gmail.com>
2024-02-15 11:18:18 -0700

    resume and pause implemented

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -6,16 +6,21 @@ export default function ScoreBoard() {
 	const dispatch = useDispatch();
 	const { score, isRunning, gameOver } = useSelector(state => state);
 
-  return (
-    <div className="score-board">
-      <div>Score:{score}</div>
-      <div>Level: 1</div>
-      <button className="score-board-button" onClick={(e) => {
-        // ...
-      }}>{isRunning ? 'Pause' : 'Play'}</button>
-      <button className="score-board-button" onClick={(e) => {
-        dispatch(restart())
-      }}>Restart</button>
-    </div>
-  )
+	return (
+		<div className="score-board">
+			<div>Score:{score}</div>
+			<div>Level: 1</div>
+			<button className="score-board-button" onClick={(e) => {
+				if (gameOver) { return }
+				if (isRunning) {
+					dispatch(pause())
+				} else {
+					dispatch(resume())
+				}
+			}}>{isRunning ? 'Pause' : 'Play'}</button>
+			<button className="score-board-button" onClick={(e) => {
+				dispatch(restart())
+			}}>Restart</button>
+		</div>
+	)
 }
